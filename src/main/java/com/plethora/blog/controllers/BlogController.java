5dd70a9347519ae0d8497ac0c22b44d138f3c0db
Rebel-cat <54984087+pal4d1n@users.noreply.github.com>
2020-10-19 21:01:13 +0300

    New functionality has been Delete,Edit

--- src/main/java/com/plethora/blog/controllers/BlogController.java
+++ src/main/java/com/plethora/blog/controllers/BlogController.java
@@ -6,10 +6,7 @@ import org.apache.catalina.filters.ExpiresFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.xml.ws.http.HTTPException;
 import java.util.ArrayList;
@@ -27,31 +24,31 @@ public class BlogController {
 
 
     @GetMapping("/blog")
-    public String blogMain(Model model){
-Iterable<Post> posts = postRepository.findAll();
-model.addAttribute("posts", posts);
+    public String blogMain(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog-main";
     }
 
     @GetMapping("/blog/add")
-    public String blogAdd(Model model){
+    public String blogAdd(Model model) {
         //Iterable<Post> posts = postRepository.findAll();
         //model.addAttribute("posts", posts);
         return "blog-add";
     }
 
     @PostMapping("/blog/add")
-    public String blogPostAdd(@RequestParam String title, String anons, String fullText, Model model ){
-        Post post = new Post(title,anons,fullText);
+    public String blogPostAdd(@RequestParam String title, String anons, String fullText, Model model) {
+        Post post = new Post(title, anons, fullText);
         postRepository.save(post);
-return "redirect:/blog";
+        return "redirect:/blog";
     }
 
     @GetMapping("/blog/{id}")
-    public String blogDetails(@PathVariable( value = "id") long id, Model model){
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
 
-        if(!postRepository.existsById(id)){
-           return "redirect:/blog";
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
         }
 
         Optional<Post> post = postRepository.findById(id);
@@ -62,17 +59,35 @@ return "redirect:/blog";
     }
 
     @GetMapping("/blog/{id}/edit")
-    public String blogEdit(@PathVariable( value = "id") long id, Model model){
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
 
-        if(!postRepository.existsById(id)){
+        if (!postRepository.existsById(id)) {
             return "redirect:/blog";
         }
 
-       // Optional<Post> post = postRepository.findById(id);
-       // ArrayList<Post> res = new ArrayList<>();
-       // post.ifPresent(res::add);
-       // model.addAttribute("post", res);
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
         return "blog-edit";
     }
 
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostEdit(@PathVariable(value = "id") long id, @RequestParam String title, String anons, String fullText, Model model) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new IllegalStateException("missing"));
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFullText(fullText);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    @DeleteMapping()
+    public String blogPostDelete(@PathVariable(value = "id") long id, Model model) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new IllegalStateException("missing"));
+        postRepository.delete(post);
+        return "redirect:/blog";
+
+    }
 }
